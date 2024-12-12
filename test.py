d2d88515a5a93ca80e9d87662b2a3c0d5b65b8e2
hanickaa <162619898+hanickaa@users.noreply.github.com>
2024-12-12 09:46:18 +0100

    prime

--- test.py
+++ test.py
@@ -18,9 +18,19 @@ def test_fibonacci(n):
         print(e)
         return None
 
+def is_prime(n):
+    if not isinstance(n, int) or n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
 if __name__ == "__main__":
     test_fibonacci(10)
     test_fibonacci(-1)
     test_fibonacci(0)
     test_fibonacci(1)
     test_fibonacci(5)
+    print(is_prime(11))
+    print(is_prime(4)) 
