1a2b94f3446f7192a05660d57a61819a48efc29c
hanickaa <162619898+hanickaa@users.noreply.github.com>
2024-12-12 09:53:11 +0100

    testr

--- test.py
+++ test.py
@@ -19,8 +19,10 @@ def test_fibonacci(n):
         return None
 
 def is_prime(n):
-    if not isinstance(n, int) or n < 2:
-        raise ValueError("n must be an integer greater than 1")
+    if not isinstance(n, int):
+        raise ValueError("n must be an integer")
+    if n < 2:
+        return False
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             return False
@@ -35,6 +37,7 @@ if __name__ == "__main__":
     try:
         print(is_prime(11))
         print(is_prime(4))
+        print(is_prime(1))
         print(is_prime(-1))
     except ValueError as e:
         print(e)
