636abe1f22ec298fee8121c67c733015d2e97b0a
hanickaa <162619898+hanickaa@users.noreply.github.com>
2025-02-27 22:13:26 +0100

    ukol pokus

---
+++ cms/my_app/views.py
@@ -0,0 +1,24 @@
+import json
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def homepage(request):
+    with open('articles.json', encoding='utf-8') as f:
+        articles = json.load(f)
+        
+    return render(request, 'my_app/homepage.html', {'articles': articles})
+    
+def article(request, id):
+    with open('articles.json', encoding='utf-8') as f:
+        articles = json.load(f)
+        
+    article = articles[id]
+    return render(request, 'my_app/article.html', {'article': article})
+    
+def hello(request):
+    return HttpResponse("Hello, Django!")
+
+def vynasob(request, a, b):
+    return HttpResponse(f'{a} * {b} = {a*b}')
