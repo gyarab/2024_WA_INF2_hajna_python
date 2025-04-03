9790b8ea86f7beba55d7559fd48965ff0be11b32
hanickaa <162619898+hanickaa@users.noreply.github.com>
2025-04-03 21:30:54 +0200

    virtual

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\hana\OneDrive\Plocha\2024_WA_INF2_hajna_python\.venv
+set VIRTUAL_ENV=C:\Users\hana\2024_WA_INF2_hajna_python\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
