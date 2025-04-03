5c5701c8ee4d2a3f258842f1839db1a19580e7c9
Hana Hajná <117266542+HanaHajna7@users.noreply.github.com>
2025-04-03 19:16:29 +0000

    aefkb

--- cms/my_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import Article, Author, Category
-
-admin.site.register(Article)
-admin.site.register(Author)
-admin.site.register(Category)
